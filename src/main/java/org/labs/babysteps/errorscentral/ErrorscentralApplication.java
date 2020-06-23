fd91d6a702610b551625f93fbf2311dc463e577e
Gilton Nascimento <gilton_nasc@yahoo.com.br>
2020-06-23 15:43:25 -0300

    Adding a project in github repository

---
+++ src/main/java/org/labs/babysteps/errorscentral/ErrorscentralApplication.java
@@ -0,0 +1,13 @@
+package org.labs.babysteps.errorscentral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErrorscentralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErrorscentralApplication.class, args);
+	}
+
+}
