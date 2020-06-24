54f48d2b074605fdf1152b32ac90c640b72cf499
Gilton Nascimento <gilton_nasc@yahoo.com.br>
2020-06-24 12:28:09 -0300

    Adding the @Service annotation for UserService and altering the method name getAllUsers() to findAll()

--- src/main/java/org/labs/babysteps/errorscentral/domain/service/UserService.java
+++ src/main/java/org/labs/babysteps/errorscentral/domain/service/UserService.java
@@ -5,14 +5,16 @@ import java.util.List;
 import org.labs.babysteps.errorscentral.domain.entity.User;
 import org.labs.babysteps.errorscentral.domain.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-public class UserService implements UserServiceInterface{
+@Service
+public class UserService implements UserServiceInterface {
 
 	@Autowired
 	private UserRepository userRepository;
 
 	@Override
-	public List<User> getAllUsers() {
+	public List<User> findAll() {
 		return userRepository.findAll();
 	}
 
